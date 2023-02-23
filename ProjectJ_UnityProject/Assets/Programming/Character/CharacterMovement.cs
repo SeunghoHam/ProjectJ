056e98b64d8f8962e9f08ba1d4a7b26db5baad3c
HAM <hamsh2003@naver.com>
2023-02-24 00:35:56 +0900

    @ 엘든링 이동방식 제작중

--- ProjectJ_UnityProject/Assets/Programming/Character/CharacterMovement.cs
+++ ProjectJ_UnityProject/Assets/Programming/Character/CharacterMovement.cs
@@ -1,32 +1,35 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UniRx;
 using Unity.VisualScripting;
-using UnityEditor.Timeline;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class CharacterMovement : MonoBehaviour
 {
-   
+
+    MouseController _mouseController;
     CharacterController controller;
-    Animator anim;
+    //Animator anim;
     Transform cam;
-    float _speedSmoothVelocity;
-    float _speedSmoothTime;
+    float _speedSmoothVelocity = 1f;
+    float _speedSmoothTime = 1f;
     float _currentSpeed;
     float _velocityY;
     Vector3 _moveInput;
     Vector3 _dir;
 
     float gravity = 25.0f;
-    float moveSpeed = 2.0f;
+    float moveSpeed = 3.0f;
     float rotateSpeed = 3.0f;
 
     [SerializeField] private GameObject _rotateActor;
+    [SerializeField] private Transform _target;
+
     void Start()
     {
+        _mouseController = this.GetComponent<MouseController>();
         controller = this.gameObject.GetComponent<CharacterController>();
         cam = Camera.main.transform;
         CursurSetting();
@@ -36,9 +39,14 @@ public class CharacterMovement : MonoBehaviour
     void Update()
     {
         GetInput();
-        PlayerMovement();
+        //PlayerMovement();
         //PlayerRotation();
         InputAction();
+
+        
+    }
+    private void LateUpdate()
+    {
     }
 
     private void CursurSetting() // 임시니까 그냥 Movement에 박음
@@ -49,24 +57,43 @@ public class CharacterMovement : MonoBehaviour
     }
     private void GetInput()
     {
-        _moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        Vector3 forward = cam.forward;
-        Vector3 right = cam.right;
+        _moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized; // 이동 각도 가져오기
+        Vector3 forward = Vector3.forward;
+        Vector3 right = Vector3.right;
         forward.y = 0;
         right.y = 0;
-        forward.Normalize();
-        right.Normalize();
-        _dir = (forward * _moveInput.y + right * _moveInput.x).normalized;
+
+        if (_moveInput.x == 0 && _moveInput.y == 0) // 입력이 없는 상태
+        {
+            _dir = Vector3.zero;
+        }
+        else // _dir 전달
+        {
+            _dir = (forward * _moveInput.y + right * _moveInput.x); // normalized 하면 1 / 0 만 존재하게 되서 딱 딱 끊김
+                                                                    // 만약 공격이나 애니메이션 동작중이 아니라면 : 조건문 추가해줘야
+
+            //Quaternion newRot = Quaternion.Euler(0, _target.transform.localRotation.y, 0); // target에서 y값만 가져오기 위해서
+            _rotateActor.transform.localRotation = Quaternion.Lerp(_rotateActor.transform.localRotation, _target.transform.localRotation, 1f);
+            // 이동은 target이 바라보는 방향으로 진행되도록
+            _currentSpeed = Mathf.SmoothDamp(_currentSpeed, moveSpeed, ref _speedSmoothVelocity, _speedSmoothTime * Time.deltaTime);
+            //Vector3 velocity = (_dir * _currentSpeed) + Vector3.up * _velocityY; // 기존 이동방식
+
+            //controller.Move(_rotateActor.transform.localRotation* direction * Time.deltaTime);
+        }
+        if (_velocityY > -10) _velocityY -= Time.deltaTime * gravity;
+        //Vector3 forward = cam.forward;
+        //Vector3 right = cam.right;
+
+        //forward.Normalize();
+        //right.Normalize();
+
+        // 이동 방향은 target의 RotValue를 기준으로 함
     }
     private void PlayerMovement()
     {
-        _currentSpeed = Mathf.SmoothDamp(_currentSpeed, moveSpeed, ref _speedSmoothVelocity, _speedSmoothTime * Time.deltaTime);
-
-        if (_velocityY > -10) _velocityY -= Time.deltaTime * gravity;
-
-        Vector3 velocity = (_dir * _currentSpeed) + Vector3.up * _velocityY;
-        controller.Move(velocity * Time.deltaTime);
+    
 
 
         // 애니메이션 적용 
--- ProjectJ_UnityProject/Assets/Programming/Character/MouseController.cs
+++ ProjectJ_UnityProject/Assets/Programming/Character/MouseController.cs
@@ -2,31 +2,45 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using UniRx.Triggers;
+using UnityEditor.Timeline;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 public class MouseController : MonoBehaviour
 {
 
     private float _rotX;
     private float _rotY;
-    private float _sensitivity = 10f;
+    private float _sensitivity = 5f;
 
     float _rotateSmooth = 1f; 
-    private Vector2 _clampAxis = new Vector2(-40,40);
+  
+    [SerializeField] private Transform _target; //  카메라가 바라보는 대상
 
-    [SerializeField] private GameObject _rotateActor;
-    void Update()
+    public Vector3 TargetValue()
+    {
+        return _target.transform.localEulerAngles;
+    }
+    private void Update()
     {
         MouseRotator();
     }
-
     private void MouseRotator()
     {
-        Vector2 mouseAxis = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-        _rotX += (mouseAxis.x * _sensitivity) * Time.deltaTime;
-        _rotY -= (mouseAxis.y * _sensitivity) * Time.deltaTime;
+        // 마우스 위로올림 x + / 마우스 아래로 내림 x -
+        // 마우스 오른쪽으로 y + / 마우스 왼쪽으로  y -
 
-        _rotY = Mathf.Clamp(_rotY, _clampAxis.x, _clampAxis.y);
+        // 마우스를 위로올리면 y가 증가
+        // 마우스를 오른쪽으로 x가 증가
+        Vector2 mouseAxis = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")); // 십자좌표계
+        _rotX += (mouseAxis.x * _sensitivity);
+        _rotY += (mouseAxis.y * _sensitivity);
+
+        _rotY = Mathf.Clamp(_rotY, -30f, 30f);
         Quaternion localRotation = Quaternion.Euler(_rotY, _rotX, 0);
-        _rotateActor.transform.rotation = Quaternion.Slerp(_rotateActor.transform.rotation, localRotation, Time.deltaTime * _rotateSmooth);
+        // 감도 설정은 나중에 다시 들어가야함
+        //_target.transform.localRotation = Quaternion.Slerp(_target.transform.localRotation, localRotation, Time.deltaTime * _rotateSmooth);
+        _target.transform.localRotation = localRotation;
     }
 }
