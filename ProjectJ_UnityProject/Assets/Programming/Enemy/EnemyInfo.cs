33e93b3280ad5983ee2df7807da36f49c3733f36
HAM <hamsh2003@naver.com>
2023-02-26 14:52:23 +0900

    # Enemy관련 데이터 및 캐릭터 핀 타겟 기능 제작

---
+++ ProjectJ_UnityProject/Assets/Programming/Enemy/EnemyInfo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[CreateAssetMenu(fileName = "EnemyData", menuName = "Enemy")]
+public class EnemyInfo : ScriptableObject
+{
+    public string _name;
+    public Rare _rare;
+    public enum Rare
+    { 
+        Normal,
+        Rare,
+        Boss,
+    }
+
+    public int _hp;
+    public int _damage;
+}
